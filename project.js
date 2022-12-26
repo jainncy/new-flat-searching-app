415fd19a5e491e6a7caf2ee6fb7f37acba87e1a5
Your Name <you@example.com>
2022-12-26 16:37:48 -0500

    first commit

---
+++ project.js
@@ -0,0 +1,7 @@
+function searchcity() {
+    var search = document.getElementById('city').value;
+    document.getElementById('search city').style.color = color; 
+    document.getElementById('search-button').style.background = color;
+    document.getElementById("search").innerHTML = color;
+  }
+  
